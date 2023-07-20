741c4c16d870e298094cb85a5d7556d8a3560ad0
Royroy205 <leroyjr2344@live.com>
2023-07-20 14:29:40 -0500

    Done With Work

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -7,4 +7,12 @@
             Console.WriteLine("Hello, World!");
         }
     }
+    string patientname = "James Will";
+    int age = 74;
+    char middleName = 'L';
+    bool RealName = true;
+    double height = 5.5;
+    decimal weight = 175.00m;
+
+    Console.WriteLine($"Custome name is {patientname}. He is {74} years old. His Middle initial is {middleName}. The accuracy is {RealName}. His height is{height}. He weight{weight}");
 }
